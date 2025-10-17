ebf084772c0c5ed3f689fbe9d73345c60757c6c6
Antonio Úbeda Montero <antonioubedamontero@gmail.com>
2025-10-17 21:24:41 +0200

    skeleton created

---
+++ src/main-curriculum/components/custom-footer.tsx
@@ -0,0 +1,7 @@
+export const CustomFooter = () => {
+  return (
+    <footer className="p-4 text-center bg-red-100 border-t-2 border-t-gray-50">
+      Custom header footer
+    </footer>
+  );
+};
--- src/main-curriculum/main-curriculum.tsx
+++ src/main-curriculum/main-curriculum.tsx
@@ -1,8 +1,23 @@
+import {
+  CustomAside,
+  CustomFooter,
+  CustomHeader,
+  CustomMain,
+} from "./components";
+
 export const MainCurriculum = () => {
   return (
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque,
-      repellendus.
-    </div>
+    <>
+      <CustomHeader />
+      <div className="flex flex-col md:flex-row">
+        <div className="bg-orange-50 md:basis-1/4 md:border-r-2 md:border-r-gray-100">
+          <CustomAside />
+        </div>
+        <div className="border-t-2 border-t-gray-100 md:basis-3/4 md:border-0">
+          <CustomMain />
+        </div>
+      </div>
+      <CustomFooter />
+    </>
   );
 };
