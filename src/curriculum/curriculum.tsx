3a2f4698d41aac9ad3416adae371372b8d13fd5b
Antonio Úbeda Montero <antonioubedamontero@gmail.com>
2025-10-17 18:51:41 +0200

    initial commit

---
+++ src/curriculum/curriculum.tsx
@@ -0,0 +1,8 @@
+export const Curriculum = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque,
+      repellendus.
+    </div>
+  );
+};
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import "./index.css";
+import { Curriculum } from "./curriculum";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <Curriculum />
+  </StrictMode>
+);
