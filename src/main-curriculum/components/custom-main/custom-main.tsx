d98245ab761c21e939f024e183d6dbf3c893b79a
Antonio Úbeda Montero <antonioubedamontero@gmail.com>
2025-10-20 12:28:18 +0200

    partial changes

--- src/main-curriculum/components/custom-main/custom-main.tsx
+++ src/main-curriculum/components/custom-main/custom-main.tsx
@@ -1,6 +1,6 @@
 import { useSection } from "../../hooks";
 import { SectionItem } from "./section-item";
-import { getIconFromIconKey } from "../../helpers/icons";
+import { SoftSkillsSection } from "./soft-skills-section";
 
 export const CustomMain = () => {
   const { sectionsData: aboutMeSectionData } = useSection("about-me");
@@ -23,26 +23,9 @@ export const CustomMain = () => {
           );
         })}
 
-      {softSkillsSection &&
-        softSkillsSection?.map((sectionItemData) => {
-          return (
-            <section className="my-4">
-              <h2 className="text-blue-700 my-4 flex gap-2 items-center text-2xl underline">
-                {getIconFromIconKey(sectionItemData.icon)}
-                <span>{sectionItemData.title}</span>
-              </h2>
-              <div className="flex gap-4 flex-wrap">
-                {sectionItemData.content.map((hability) => {
-                  return (
-                    <div className="p-1 rounded-xl bg-indigo-400 text-white">
-                      {hability}
-                    </div>
-                  );
-                })}
-              </div>
-            </section>
-          );
-        })}
+      {softSkillsSection && (
+        <SoftSkillsSection softSkillsSection={softSkillsSection} />
+      )}
     </>
   );
 };
